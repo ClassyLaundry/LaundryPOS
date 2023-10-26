1396791cbb552e4ca3eac2021e4391a9d269c7d3
Mitchell Arthur <64058372+Mitchell-Arthur@users.noreply.github.com>
2023-10-26 22:35:58 +0700

    Update Kitir.blade.php
    Revisi ukuran kitir

--- resources/views/pages/print/Kitir.blade.php
+++ resources/views/pages/print/Kitir.blade.php
@@ -7,13 +7,12 @@
         /* Add any styles you want to use for the PDF here */
         body {
             font-family: sans-serif;
-            font-size: 10pt
         }
     </style>
 </head>
 <body style="width: 100%; height: 100%;">
     @for($i = 0; $i < $cetak; $i++)
-        <h3 style="position: absolute; left: 0px; top: {{ $i * 70 - 30 }}px; width: 100%;">{{ $data->kitir_code }}</h3>
+        <h3 style="position: absolute; left: 0px; top: {{ $i * 70 - 30 }}px; width: 100%; font-size: 1.25rem; font-weight: bold;">{{ $data->kitir_code }}</h3>
     @endfor
 </body>
 
