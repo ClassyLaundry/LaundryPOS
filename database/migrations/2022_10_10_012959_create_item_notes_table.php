d915a612a1f0386de08db6e9b4ae2e538b84e1a4
Mitchell Arthur <mitchell2@mhs.stts.edu>
2023-12-01 14:28:05 +0700

    Update 2022_10_10_012959_create_item_notes_table.php
    change image path to text

--- database/migrations/2022_10_10_012959_create_item_notes_table.php
+++ database/migrations/2022_10_10_012959_create_item_notes_table.php
@@ -27,7 +27,7 @@ return new class extends Migration
             $table->boolean('back_top_right')->default(0);
             $table->boolean('back_bottom_left')->default(0);
             $table->boolean('back_bottom_right')->default(0);
-            $table->string('image_path')->nullable();
+            $table->text('image_path')->nullable();
             $table->foreignId('modified_by')
                 ->constrained('users', 'id')
                 ->cascadeOnDelete();
