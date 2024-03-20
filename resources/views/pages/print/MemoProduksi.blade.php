0b74dab705da75c9ec263f5fdb28a32bfe6a7640
Mitchell Arthur <mitchell2@mhs.stts.edu>
2024-03-20 15:20:00 +0700

    remove margin

--- resources/views/pages/print/MemoProduksi.blade.php
+++ resources/views/pages/print/MemoProduksi.blade.php
@@ -45,7 +45,7 @@
         }
     </style>
 </head>
-<body class="lh-1 px-2" style="width: 900px; margin-top: 100px;">
+<body class="lh-1" style="width: 900px; margin-top: 100px;">
     <div id="data-header">
         <p class="fw-bold fs-6">
             {{ Str::upper($data->transaksi->outlet->nama) }}<br>
