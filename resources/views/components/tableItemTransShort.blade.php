f7f4fe8df9a2db00eeab575590613d9a46b041bd
Mitchell Arthur <mitchellarthurvittorio@gmail.com>
2024-11-07 15:10:21 +0700

    add qty to detail transaksi proses

--- resources/views/components/tableItemTransShort.blade.php
+++ resources/views/components/tableItemTransShort.blade.php
@@ -56,6 +56,7 @@
             <tr>
                 <th style="width: 62.5%;">Nama Item</th>
                 <th style="width: 20%;">Kategori</th>
+                <th style="width: 7.5%;">Qty</th>
                 @if(Session::get('role') != 'delivery')
                 <th class="column-action"></th>
                 @endif
@@ -66,6 +67,7 @@
                 <tr id="trans-{{ $trans->id }}">
                     <td style="width: 62.5%;">{{ $trans->item_transaksi[$i]->nama }}</td>
                     <td style="width: 20%;" class="text-center">{{ $trans->item_transaksi[$i]->nama_kategori }}</td>
+                    <td style="width: 7.5%;" class="text-center">{{ $item_transaksi->qty }}</td>
                     <td class="cell-action">
                         <div class="d-flex h-100 align-items-center justify-content-end">
                             @if(isset($rewashes))
