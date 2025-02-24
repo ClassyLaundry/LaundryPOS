74c5a19f1087eb8839e064d7de90e17cdd09f684
Mitchell Arthur <mitchellarthurvittorio@gmail.com>
2025-02-24 13:45:42 +0700

    Update tableLaporanKas.blade.php

--- resources/views/components/tableLaporanKas.blade.php
+++ resources/views/components/tableLaporanKas.blade.php
@@ -20,15 +20,15 @@
                 @endphp
                 @foreach ($kas as $data)
                 @php
-                    if ($tipe != $data->tipe) {
-                        $tipe = $data->tipe;
+                    if ($tipe != $data["tipe"]) {
+                        $tipe = $data["tipe"];
                         $index = 0;
                     } else {
                         $index++;
                     }
                 @endphp
                     @if ($index == 0)
-                        <tr><td rowspan="{{ $rowHeight[$tipe] == 1 ? $rowHeight[$tipe] : $rowHeight[$data->tipe] + 1 }}" class="table-primary">{{ Str::upper($data->tipe) }}</td>
+                        <tr><td rowspan="{{ $rowHeight[$tipe] == 1 ? $rowHeight[$tipe] : $rowHeight[$data["tipe"]] + 1 }}" class="table-primary">{{ Str::upper($data["tipe"]) }}</td>
                     @else
                         <tr>
                     @endif
@@ -45,13 +45,13 @@
                         <td>{{ $data->keterangan }}</td>
                         <td>{{ $data->operator }}</td>
                     </tr>
-                    @if ($rowHeight[$data->tipe] == $index + 1 && $index != 0)
+                    @if ($rowHeight[$data["tipe"]] == $index + 1 && $index != 0)
                         <tr class="table-primary">
-                            <td colspan="4" class="text-center">{{ "TOTAL KAS MASUK VIA " . Str::upper($data->tipe) }}</td>
+                            <td colspan="4" class="text-center">{{ "TOTAL KAS MASUK VIA " . Str::upper($data["tipe"]) }}</td>
                             <td>
                                 <div class="d-flex justify-content-between">
                                     <span>Rp</span>
-                                    <span>{{ number_format($sumOfEachPaymentMethod[$data->tipe], 0, ',', '.') }}</span>
+                                    <span>{{ number_format($sumOfEachPaymentMethod[$data["tipe"]], 0, ',', '.') }}</span>
                                 </div>
                             </td>
                             <td colspan="2"></td>
